3bd7fad1bf32783b32e70f19a70aebd7b4e1466e
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-12-19 21:48:58 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,6 +1,6 @@
 {
   "isCheckWhiteList": 0,
-  "start":             "Dec 20 2021 05:00:00 GMT",
+  "start":             "Dec 19 2021 05:00:00 GMT",
   "end_round_1":       "Dec 20 2021 07:00:00 GMT",
   "end_round_2":       "Dec 20 2021 09:00:00 GMT",
   "end_waiting_claim": "Dec 20 2021 17:00:00 GMT"
